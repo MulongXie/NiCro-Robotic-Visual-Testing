41024479266161adb4eed8e863e7d2c889a791a2
MulongXie <dsh15325@163.com>
2022-02-25 11:20:25 +1100

    correct the timer

--- detect_compo/ip_region_proposal.py
+++ detect_compo/ip_region_proposal.py
@@ -38,7 +38,7 @@ def nesting_inspection(org, grey, compos, ffl_block):
 def compo_detection(input_img_path, output_root, uied_params,
                     adaptive_binarization=False, resize_by_height=800, classifier=None, show=False, wai_key=0):
 
-    start = time.clock()
+    start = time.time()
     name = input_img_path.replace('\\', '/').split('/')[-1][:-4]
     ip_root = file.build_directory(pjoin(output_root, "ip"))
 
@@ -90,5 +90,5 @@ def compo_detection(input_img_path, output_root, uied_params,
     # *** Step 7 *** save detection result
     Compo.compos_update(uicompos, org.shape)
     file.save_corners_json(pjoin(ip_root, name + '.json'), uicompos)
-    print("[Compo Detection Completed in %.3f s] Input: %s Output: %s" % (time.clock() - start, input_img_path, pjoin(ip_root, name + '.json')))
+    print("[Compo Detection Completed in %.3f s] Input: %s Output: %s" % (time.time() - start, input_img_path, pjoin(ip_root, name + '.json')))
     return board
