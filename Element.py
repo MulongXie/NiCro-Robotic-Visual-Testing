afd5e86038a306ae16ba5ecfbcf107b224fa4a99
MulongXie <dsh15325@163.com>
2022-03-01 16:31:51 +1100

    resize bound by different sides

--- Element.py
+++ Element.py
@@ -34,16 +34,19 @@ class Element:
         left, right, top, bottom = int(self.col_min * ratio), int(self.col_max * ratio), int(self.row_min * ratio), int(self.row_max * ratio)
         self.clip = org_img[top: bottom, left: right]
 
-    def resize_bound(self, resize_ratio):
-        return [int(self.col_min*resize_ratio), int(self.row_min*resize_ratio), int(self.col_max*resize_ratio), int(self.row_max*resize_ratio)]
+    def resize_bound(self, resize_ratio_col, resize_ratio_row):
+        return [int(self.col_min*resize_ratio_col), int(self.row_min*resize_ratio_row), int(self.col_max*resize_ratio_col), int(self.row_max*resize_ratio_row)]
+
+    def get_bound(self):
+        return self.col_min, self.row_min, self.col_max, self.row_max
 
     def draw_element(self, board, ratio=None, color=(0,255,0), line=2, show_id=True, show=False):
         if not ratio:
             ratio = board.shape[0] / self.detection_img_size[0]
-        bound = self.resize_bound(ratio)
+        bound = self.resize_bound(ratio, ratio)
         cv2.rectangle(board, (bound[0], bound[1]), (bound[2], bound[3]), color, line)
         if show_id:
-            cv2.putText(board, self.id, (bound[0] + 3, bound[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+            cv2.putText(board, str(self.id), (bound[0] + 3, bound[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
         if show:
             cv2.imshow(self.id, cv2.resize(board, (int(board.shape[1] * (800 / board.shape[0])), 800)))
             cv2.waitKey()
