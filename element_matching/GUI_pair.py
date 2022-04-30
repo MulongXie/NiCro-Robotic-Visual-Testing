eb98bb85e7400d3f69ad6993aa2519d0b9cd7106
MulongXie <dsh15325@163.com>
2022-04-30 10:26:53 +1000

    sort the matched elements by similarity

--- element_matching/GUI_pair.py
+++ element_matching/GUI_pair.py
@@ -45,39 +45,49 @@ class GUIPair:
         else:
             target_ele_text = sorted(target_ele_text, key=lambda x: len(x), reverse=True)
         matched_elements = []
+        similarities = []
         is_matched = False
         for tar in target_ele_text:
             for text_ele in compare_elements:
                 sim = SequenceMatcher(None, text_ele.text_content, tar).ratio()
                 if sim > self.min_similarity_text:
                     matched_elements.append(text_ele)
+                    similarities.append(sim)
                     is_matched = True
             # only use the longest matched text
             if is_matched:
                 break
+        # sort by similarity
+        sorted_id = np.argsort(similarities)[::-1]
+        matched_elements = matched_elements[sorted_id]
         return matched_elements
 
-    def match_by_img(self, target_element, compared_elements, hash_check=True):
+    def match_by_img(self, target_element, compared_elements, hash_check=False):
         # similarities between the target element and all elements in gui2
         resnet_sims = matching.image_similarity_matrix([target_element.clip], [e.clip for e in compared_elements], method='resnet', resnet_model=self.resnet_model)[0]
         # filter by similarity threshold
         matched_elements_id = np.where(resnet_sims > self.min_similarity_img)[0]
+        # sort by similarity
+        sorted_id = np.argsort(resnet_sims[matched_elements_id])[::-1]  # get the index
+        matched_elements_id = matched_elements_id[sorted_id]
         # select from the compared_elements
         matched_elements = np.array(compared_elements)[matched_elements_id]
         self.show_target_and_matched_elements(target_element, matched_elements, similarities=resnet_sims[matched_elements_id])
 
         # double check by dhash
         if hash_check and len(matched_elements) > 0:
-            dhash_sims= matching.image_similarity_matrix([target_element.clip], [e.clip for e in matched_elements], method='dhash')[0]
+            dhash_sims = matching.image_similarity_matrix([target_element.clip], [e.clip for e in matched_elements], method='dhash')[0]
             matched_elements_id = np.where(dhash_sims > self.min_similarity_img)[0]
+            sorted_id = np.argsort(dhash_sims[matched_elements_id])[::-1]  # get the index
+            matched_elements_id = matched_elements_id[sorted_id]
             matched_elements = matched_elements[matched_elements_id]
-            self.show_target_and_matched_elements(target_element, matched_elements, similarities=dhash_sims[matched_elements_id])
+            # self.show_target_and_matched_elements(target_element, matched_elements, similarities=dhash_sims[matched_elements_id])
         return matched_elements
 
     def match_by_shape(self, target_element, compared_elements):
         matched_elements = []
         for ele in compared_elements:
-            if (min(target_element.aspect_ratio / ele.aspect_ratio) / max(target_element.aspect_ratio, ele.aspect_ratio)) > self.min_shape_difference:
+            if (min(target_element.aspect_ratio, ele.aspect_ratio) / max(target_element.aspect_ratio, ele.aspect_ratio)) > self.min_shape_difference:
                 matched_elements.append(ele)
         return matched_elements
 
@@ -91,17 +101,16 @@ class GUIPair:
         if target_element.category == 'Text' or target_element.text_content is not None:
             matched_elements = self.match_by_text(target_element, self.gui2.ele_texts)
             if len(matched_elements) > 1:
+                matched_elements = self.match_by_shape(target_element, matched_elements)
+            if len(matched_elements) > 1:
                 matched_elements = self.match_by_img(target_element, matched_elements)
-            if len(matched_elements) > 1:
-                matched_elements = self.match_by_shape(target_element, matched_elements)
-            if len(matched_elements) > 1:
-                matched_elements = self.match_by_neighbour(target_element, matched_elements)
+            # if len(matched_elements) > 1:
+            #     matched_elements = self.match_by_neighbour(target_element, matched_elements)
         else:
-            matched_elements = self.match_by_img(target_element, self.gui2.ele_compos)
-            if len(matched_elements) > 1:
-                matched_elements = self.match_by_shape(target_element, matched_elements)
-            if len(matched_elements) > 1:
-                matched_elements = self.match_by_neighbour(target_element, matched_elements)
+            matched_elements = self.match_by_shape(target_element, self.gui2.ele_compos)
+            matched_elements = self.match_by_img(target_element, matched_elements)
+            # if len(matched_elements) > 1:
+            #     matched_elements = self.match_by_neighbour(target_element, matched_elements)
         return matched_elements[0]
 
     '''
@@ -130,5 +139,5 @@ class GUIPair:
         cv2.imshow('Target', board1)
         cv2.imshow('Matched Elements', board2)
         cv2.waitKey()
-        # cv2.destroyWindow('Target')
-        # cv2.destroyWindow('Matched Elements')
+        cv2.destroyWindow('Target')
+        cv2.destroyWindow('Matched Elements')
