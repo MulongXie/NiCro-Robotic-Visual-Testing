9d2ab62e01eacca3b741f71e721555ebc4e25a34
MulongXie <dsh15325@163.com>
2022-05-05 19:28:26 +1000

    add pad in element clipping

--- element_detection/Element.py
+++ element_detection/Element.py
@@ -29,12 +29,12 @@ class Element:
         self.aspect_ratio = round(self.width / self.height)
         self.area = self.width * self.height
 
-    def get_clip(self, org_img, bias=3):
+    def get_clip(self, org_img, pad=3):
         left, right, top, bottom = int(self.col_min), int(self.col_max), int(self.row_min), int(self.row_max)
-        left = max(0, left - bias)
-        right = min(org_img.shape[1], right + bias)
-        top = max(0, top - bias)
-        bottom = min(org_img.shape[0], bottom + bias)
+        left = max(0, left - pad)
+        right = min(org_img.shape[1], right + pad)
+        top = max(0, top - pad)
+        bottom = min(org_img.shape[0], bottom + pad)
         self.clip = org_img[top: bottom, left: right]
 
     def resize_bound(self, resize_ratio_col, resize_ratio_row):
